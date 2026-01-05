8983b4aeeed0f1c1e18c74de54f87a26fb37f665
Fernando Cañas <fernando.canas@outlook.com>
2026-01-05 18:07:52 -0500

    feat: return all users and user by id

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/wfercanas/kakebook-server
 
 go 1.23.3
+
+require github.com/google/uuid v1.6.0 // indirect
--- main.go
+++ main.go
@@ -4,15 +4,32 @@ import (
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/google/uuid"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from kakebook"))
 }
 
+func users(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Returning all users..."))
+}
+
+func userById(w http.ResponseWriter, r *http.Request) {
+	id, err := uuid.Parse(r.PathValue("userID"))
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
+	w.Write([]byte("Returning the specific user with ID: " + id.String()))
+}
+
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
+	mux.HandleFunc("/{$}", home)
+	mux.HandleFunc("/users", users)
+	mux.HandleFunc("/users/{userID}", userById)
 
 	log.Print("Starting server on :4000")
 
