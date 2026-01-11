541f5f5ce3992dabe2977c8228a96e49085c45f5
Fernando Cañas <fernando.canas@outlook.com>
2026-01-10 22:16:09 -0500

    feat: move routes to routes.go

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -19,16 +19,9 @@ func main() {
 		logger: slog.New(slog.NewTextHandler(os.Stdout, nil)),
 	}
 
-	mux := http.NewServeMux()
-	mux.HandleFunc("GET /{$}", app.home)
-	mux.HandleFunc("GET /users", app.users)
-	mux.HandleFunc("GET /users/{userID}", app.getUserById)
-
-	mux.HandleFunc("POST /users", app.createNewUser)
-
 	app.logger.Info("starting server", slog.String("addr", *addr))
 
-	err := http.ListenAndServe(*addr, mux)
+	err := http.ListenAndServe(*addr, app.routes())
 	if err != nil {
 		app.logger.Error(err.Error())
 		os.Exit(1)
---
+++ cmd/web/routes.go
@@ -0,0 +1,15 @@
+package main
+
+import "net/http"
+
+func (app *application) routes() *http.ServeMux {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /{$}", app.home)
+	mux.HandleFunc("GET /users", app.users)
+	mux.HandleFunc("GET /users/{userID}", app.getUserById)
+
+	mux.HandleFunc("POST /users", app.createNewUser)
+
+	return mux
+}
