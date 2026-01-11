72a55509352f12fcaeb7784314b3da6a06d951fd
Fernando Cañas <fernando.canas@outlook.com>
2026-01-10 22:01:29 -0500

    feat: use logger from app struct in handlers

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,31 +2,37 @@ package main
 
 import (
 	"fmt"
+	"log/slog"
 	"net/http"
 
 	"github.com/google/uuid"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Server", "Go")
+	app.logger.Info(http.StatusText(http.StatusOK), slog.String("method", r.Method), slog.String("uri", r.URL.RequestURI()))
 	fmt.Fprintf(w, "Hello from kakebook")
 }
 
-func users(w http.ResponseWriter, r *http.Request) {
+func (app *application) users(w http.ResponseWriter, r *http.Request) {
+	app.logger.Info(http.StatusText(http.StatusOK), slog.String("method", r.Method), slog.String("uri", r.URL.RequestURI()))
 	fmt.Fprint(w, "Returning all users...")
 }
 
-func getUserById(w http.ResponseWriter, r *http.Request) {
+func (app *application) getUserById(w http.ResponseWriter, r *http.Request) {
 	id, err := uuid.Parse(r.PathValue("userID"))
 	if err != nil {
+		app.logger.Error(err.Error(), slog.String("method", r.Method), slog.String("uri", r.URL.RequestURI()))
 		http.NotFound(w, r)
 		return
 	}
 
+	app.logger.Info(http.StatusText(http.StatusOK), slog.String("method", r.Method), slog.String("uri", r.URL.RequestURI()))
 	fmt.Fprintf(w, "Returning the specific user with ID: %s", id.String())
 }
 
-func createNewUser(w http.ResponseWriter, r *http.Request) {
+func (app *application) createNewUser(w http.ResponseWriter, r *http.Request) {
+	app.logger.Info(http.StatusText(http.StatusOK), slog.String("method", r.Method), slog.String("uri", r.URL.RequestURI()))
 	w.WriteHeader(http.StatusCreated)
 	fmt.Fprint(w, "Created a new user...")
 }
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -15,16 +15,16 @@ func main() {
 	addr := flag.String("addr", ":4000", "HTTP Address")
 	flag.Parse()
 
-	app := application{
+	app := &application{
 		logger: slog.New(slog.NewTextHandler(os.Stdout, nil)),
 	}
 
 	mux := http.NewServeMux()
-	mux.HandleFunc("GET /{$}", home)
-	mux.HandleFunc("GET /users", users)
-	mux.HandleFunc("GET /users/{userID}", getUserById)
+	mux.HandleFunc("GET /{$}", app.home)
+	mux.HandleFunc("GET /users", app.users)
+	mux.HandleFunc("GET /users/{userID}", app.getUserById)
 
-	mux.HandleFunc("POST /users", createNewUser)
+	mux.HandleFunc("POST /users", app.createNewUser)
 
 	app.logger.Info("starting server", slog.String("addr", *addr))
 
