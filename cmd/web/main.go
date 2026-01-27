b8928a327297464ae17a2ad1378fcbd3d312b25a
Fernando Cañas <fernando.canas@outlook.com>
2026-01-26 22:00:50 -0500

    feat: connect server to database

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,29 +1,60 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
+	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
+
+	_ "github.com/lib/pq"
 )
 
 type application struct {
 	logger *slog.Logger
+	db     *sql.DB
 }
 
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP Address")
+	dsn := flag.String("dsn", "user=kakebook password=kakebook host=localhost port=5432 dbname=kakebook sslmode=disable", "Data Source Name")
 	flag.Parse()
 
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	db, err := openDB(*dsn)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+
+	defer db.Close()
+
 	app := &application{
-		logger: slog.New(slog.NewTextHandler(os.Stdout, nil)),
+		logger: logger,
+		db:     db,
 	}
 
 	app.logger.Info("starting server", slog.String("addr", *addr))
 
-	err := http.ListenAndServe(*addr, app.routes())
+	err = http.ListenAndServe(*addr, app.routes())
 	if err != nil {
-		app.logger.Error(err.Error())
+		logger.Error(err.Error())
 		os.Exit(1)
 	}
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, fmt.Errorf("error opening database: %w", err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		defer db.Close()
+		return nil, fmt.Errorf("error connecting to database: %w", err)
+	}
+
+	return db, nil
 }
--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/wfercanas/kakebook-server
 go 1.23.3
 
 require github.com/google/uuid v1.6.0
+
+require github.com/lib/pq v1.10.9
