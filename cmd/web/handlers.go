acd637aa999108545fadceb4ae730ca729a62a97
Fernando Cañas <fernando.canas@outlook.com>
2026-01-28 18:55:52 -0500

    feat: create new entry with hardcoded accounts using request body

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log/slog"
 	"net/http"
+	"time"
 
 	"github.com/google/uuid"
 )
@@ -41,4 +43,35 @@ func (app *application) createNewUser(w http.ResponseWriter, r *http.Request) {
 	app.logger.Info(http.StatusText(http.StatusOK), slog.String("method", r.Method), slog.String("uri", r.URL.RequestURI()))
 	w.WriteHeader(http.StatusCreated)
 	fmt.Fprint(w, "Created a new user...")
+}
+
+type NewEntry struct {
+	ProjectId   uuid.UUID `json:"project_id"`
+	Date        string    `json:"date"`
+	Description string    `json:"description"`
+	Amount      float32   `json:"amount"`
+}
+
+func (app *application) createNewEntry(w http.ResponseWriter, r *http.Request) {
+	var entry NewEntry
+	err := json.NewDecoder(r.Body).Decode(&entry)
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
+
+	date, err := time.Parse("2006-01-02", entry.Date)
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
+
+	err = app.entries.Insert(date, entry.Description, entry.ProjectId, entry.Amount)
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
+
+	app.logger.Info(http.StatusText(http.StatusOK), slog.String("method", r.Method), slog.String("uri", r.URL.RequestURI()))
+	w.WriteHeader(http.StatusCreated)
 }
--- internal/model/entries.go
+++ internal/model/entries.go
@@ -13,6 +13,7 @@ type Entry struct {
 	description string
 	projectId   uuid.UUID
 	amount      float32
+	movements   []Movement
 }
 
 type EntryModel struct {
@@ -21,4 +22,47 @@ type EntryModel struct {
 
 func (m *EntryModel) Get(entryId uuid.UUID) (Entry, error) {
 	return Entry{}, nil
+}
+
+func (m *EntryModel) Insert(date time.Time, description string, projectId uuid.UUID, amount float32) error {
+	entryStmt := `INSERT INTO entries (entry_id, date, description, project_id, amount)
+	VALUES (gen_random_uuid(), $1, $2, $3, $4) RETURNING entry_id`
+
+	movementStmt := `INSERT INTO movements (value, movement_type, account_id, entry_id)
+	VALUES ($1, $2, $3, $4)`
+
+	tx, err := m.DB.Begin()
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback()
+
+	var entryId uuid.UUID
+
+	err = tx.QueryRow(entryStmt, date, description, projectId, amount).Scan(&entryId)
+	if err != nil {
+		return err
+	}
+
+	bancolombiaId, err := uuid.Parse("418d3211-fb86-44e4-b83f-309107e51473")
+	if err != nil {
+		return err
+	}
+	nuId, err := uuid.Parse("e1d7c3ac-4156-4b19-97e2-782097b14e60")
+	if err != nil {
+		return err
+	}
+
+	_, err = tx.Exec(movementStmt, amount, "debit", bancolombiaId, entryId)
+	if err != nil {
+		return err
+	}
+
+	_, err = tx.Exec(movementStmt, amount, "credit", nuId, entryId)
+	if err != nil {
+		return err
+	}
+
+	tx.Commit()
+	return nil
 }
